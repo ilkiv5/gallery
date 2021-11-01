6f8dfbc6035103de961a1847c431c46afb235e69
Viktor Ilkiv <viktori.fluvius@gmail.com>
2021-11-01 12:02:51 +0200

    add material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './App.css';
+import Header from "./components/Header";
+import Buttons from "./components/Buttons";
+import Slider from "./components/Slider";
+import Dialog from "./components/Dialog"
+import ImageList from "./components/ImageList";
 
 function App() {
     return (
         <div className="App">
-            working
+            <Header/>
+            <Buttons/>
+            <Slider/>
+            <Dialog/>
+            <ImageList/>
         </div>
     );
 }
