a39d20ad43988c7dd1051e154e124edcd074dc8e
Viktor Ilkiv <viktori.fluvius@gmail.com>
2021-11-01 15:57:21 +0200

    add images

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './App.css';
 import Header from "./components/Header";
-import Buttons from "./components/Buttons";
-import Slider from "./components/Slider";
-import Dialog from "./components/Dialog"
-import ImageList from "./components/ImageList";
+
+import axios from "axios";
+
 
 function App() {
+    const [image, setImage] = useState([])
+
+    async function fetchImage() {
+        const response = await axios.get('https://pixabay.com/api/?key=24131407-f84ef8e40fb94c1e54ac238f3&q=yellow+flowers&image_type=photo&pretty=true')
+        const urlImage = response.data.hits.map(item => item.previewURL)
+        const renderImage = urlImage.map((image, index) => {
+            return <img key={index} src={image} alt="dsa"/>
+        })
+        setImage(renderImage)
+    }
+
+    useEffect(()=>{
+        fetchImage()
+    },[])
+
     return (
         <div className="App">
             <Header/>
-            <Buttons/>
-            <Slider/>
-            <Dialog/>
-            <ImageList/>
+            <div style={{marginTop:'20px'}}>
+                {image}
+            </div>
+
         </div>
     );
 }
