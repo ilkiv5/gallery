93414e14ea0c36a4d047a55b52a8b820b8b22404
Viktor Ilkiv <viktori.fluvius@gmail.com>
2021-11-04 14:04:15 +0200

    delete const className

--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -4,10 +4,10 @@ import cl from './Modal.module.css'
 
 const Modal = ({children, visible, handleSetVisible}) => {
 
-const classNames = cn(cl.dialog, visible && cl.active)
-
     return (
-        <div className={classNames} onClick={()=>handleSetVisible(false)}>
+        <div
+            className={cn(cl.dialog, visible && cl.active)}
+            onClick={()=>handleSetVisible(false)}>
             <div className={cl.dialogContent} onClick={(e)=>e.stopPropagation()}>
                 {children}
             </div>
